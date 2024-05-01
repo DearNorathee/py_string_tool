41f0cff7fc1a5c5504425d8f51bc931716468f43
NoratheeDear <noratee5@hotmail.com>
2024-05-01 10:27:11 +0700

    Import clean_filename and update detect_language

--- utils.py
+++ utils.py
@@ -1,18 +1,71 @@
 from difflib import get_close_matches
 from difflib import SequenceMatcher
 from thefuzz import fuzz
+from typing import Union, Literal
+
+def clean_filename(ori_name):
+    # update01: deal with '\n' case
+    # imported from NLP 01/NLP 03_11LabsBulk
+    replace_with_empty = [".","?",":",'"' , "\\" ] 
+    replace_with_space = ["\n", "/" ]
+    
+    new_name = ori_name
+    for delimiter in replace_with_empty:
+        new_name = new_name.replace(delimiter, "")
+        
+    for delimiter in replace_with_space:
+        new_name = new_name.replace(delimiter, " ")
+
+    return new_name
 
 def replace_backslash(s: str):
     return s.replace('\\','/')
 
-def detect_language(input_text):
+def detect_language(input_text: Union[str,list[str]], 
+                    return_as: Literal["full_name","2_chr_code","3_chr_code","langcodes_obj"] = "full_name"):
+    import pandas as pd
     from langdetect import detect
-    try:
-        # Detect the language of the text
-        language = detect(input_text)
-        return language
-    except Exception as e:
-        return f"Language detection failed: {str(e)}"
+    import langcodes
+    # medium tested
+    # wrote < 30 min(with testing)
+    # imported from C:\Users\Heng2020\OneDrive\D_Code\Python\Python NLP\NLP 02\NLP_2024\NLP 11_Local_TTS
+    if isinstance(input_text, str):
+    # assume only 1d list
+        try:
+            # Detect the language of the text
+            # language_code is 2 character code
+            lang_code_2chr = detect(input_text)
+            language_obj = langcodes.get(lang_code_2chr)
+            language_name = language_obj.display_name()
+            lang_code_3chr = language_obj.to_alpha3()
+
+            
+            if return_as in ["full_name"]:
+                ans = language_name
+            elif return_as in ["2_chr_code"]:
+                ans = lang_code_2chr
+            elif return_as in ["3_chr_code"]:
+                ans = lang_code_3chr
+            elif return_as in ["langcodes_obj"]:
+                ans = language_obj
+
+            return ans
+        except Exception as e:
+            err_str = f"Language detection failed: {str(e)}"
+            return False
+        
+    elif isinstance(input_text, list):
+        out_list = []
+        for text in input_text:
+            detect_lang = detect_language(text, return_as = return_as)
+            out_list.append(detect_lang)
+        return out_list
+    elif isinstance(input_text, pd.Series):
+        # not tested this part yet
+        unique_text = pd.Series(input_text.unique())
+        full_text = unique_text.str.cat(sep=' ')
+        detect_lang = detect_language(full_text,return_as)
+        return detect_lang
 
 def similar_score(word_in, compare_list, cut_off=0,return_word=True,return_score=False):
     # Assume that word_in is only string
