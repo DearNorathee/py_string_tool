ad1f2fadc128133c48c220c9111eb4d128d2e912
NoratheeDear <noratee5@hotmail.com>
2024-05-25 10:58:39 +0700

    merge file M01_String into utils_pst

--- utils_pst.py
+++ utils_pst.py
@@ -4,6 +4,82 @@ from thefuzz import fuzz
 from typing import Union, Literal
 import pandas as pd
 
+
+def similar_score(word_in, compare_list, cut_off=0,return_word=True,return_score=False):
+    # Assume that word_in is only string
+    from thefuzz import fuzz
+    outlist = []
+    for text in compare_list:
+        similar_score = fuzz.WRatio(word_in,text)
+        string_similar = (text,similar_score)
+        outlist.append(string_similar)
+    outlist.sort(key = lambda x:x[1],reverse=True)
+    return outlist
+
+
+
+def similar_text(word_in, compare_list, cut_off=0,return_word=True,return_score=False):
+    score_list = []
+    similar_word = []
+    if isinstance(word_in,str):
+        # if word_in is only a string
+        word_list = [word_in]
+    else:
+        # if this is a list
+        word_list = [word for word in word_in]
+    
+    for word in word_list:
+        max_score = 0
+        most_similar = ""
+        for compare_word in compare_list:
+            score = SequenceMatcher(None, str(word), compare_word).ratio()
+            if score > max_score:
+                max_score = score
+                most_similar = compare_word
+            if max_score >= cut_off:
+                score_list.append(format(max_score,".4f"))
+                similar_word.append(most_similar)
+            else:
+                score_list.append("")
+    
+    if return_word:
+        if return_score:
+            # return both word & score
+
+            if isinstance(word_in,str):
+                out_list = list(zip(similar_word,score_list))[0]
+                if len(similar_word) == 0:
+                    return ""
+            else:
+                if len(similar_word) == 0:
+                    return []
+                out_list = list(zip(similar_word,score_list))
+        else:
+            # return only word
+            out_list = similar_word
+            
+            if isinstance(word_in,str):
+                if len(similar_word) == 0:
+                    return ""
+                out_list = similar_word[0]
+            else:
+                if len(similar_word) == 0:
+                    return []
+                out_list = similar_word
+    else:
+        if return_score:
+            # return only score
+            
+            if isinstance(word_in,str):
+                out_list = score_list[0]
+            else:
+                out_list = score_list
+
+        else:
+            out_list = "Invalid! return_word & return_score can't be False at the same time"
+
+    return out_list
+
 def split_sentence(text, delimiter, inplace=True):
     """
     Split elements of a list of strings using a specified delimiter and optionally modify the list in place.
@@ -73,13 +149,40 @@ def remove_from_list(lst, char="♪"):
     # remove_from_list(lst,start_with = None,end_with = None,logic = "or")
     return [element for element in lst if not str(element).startswith(char)]
 
-def text_after(text,delimiter):
-    after_part = text.split(delimiter)[-1]
-    return after_part
+def text_after(text, prefix_list, return_as_empty=True, include_delimiter=False):
+    if isinstance(prefix_list, str):
+        prefix_list = [prefix_list]
+    
+    for prefix in prefix_list:
+        index = text.find(prefix)
+        if index != -1:
+            out_str = text[index + len(prefix):]
+            if include_delimiter:
+                out_str = prefix + out_str
+            return out_str
 
-def text_before(text,delimiter):
-    before_part = text.split(delimiter)[0]
-    return before_part
+    if return_as_empty:
+        return ""
+    else:
+        return text
+
+def text_before(text, suffix_list, return_as_empty=True, include_delimiter=False):
+    if isinstance(suffix_list, str):
+        suffix_list = [suffix_list]
+    
+    for suffix in suffix_list:
+        index = text.find(suffix)
+        if index != -1:
+            out_str = text[:index + len(suffix)]
+            if include_delimiter:
+                out_str = suffix + out_str
+            return out_str
+
+    if return_as_empty:
+        return ""
+    else:
+        return text
+
 
 def replace(text,to_replace,replace_by):
     # unit_tested
